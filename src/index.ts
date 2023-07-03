5051ef20736690d3cd37e0c85a895b25846f5623
Richard Zheng <rjzhengweb@gmail.com>
2023-07-03 12:07:00 -0700

    v0.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import useDeletedAtNull from "./rules/use-deletedAt-null";
 
 module.exports = {
+  name: "@yoke-global/prisma-soft-delete",
+  version: "0.0.2",
   rules: {
     "use-deletedAt-null": useDeletedAtNull,
   },
